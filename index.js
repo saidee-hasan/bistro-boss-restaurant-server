e004330f969a400dfd88c13ec1ff59d874313697
saideeWD <mdsaideehasan@gmail.com>
2025-01-09 10:01:15 +0600

    users sestem done

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 const cookieParser = require('cookie-parser');
 app.use(cookieParser())
-
+app.use(cors())
 require("dotenv").config();
 
  
 
 
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.cnyhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@bistro-boss.mtnkq.mongodb.net/?retryWrites=true&w=majority&appName=bistro-boss`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,9 +24,48 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const Collection = client.db("bistro").collection("boss");
+    const menuCollection = client.db("bistro").collection("menu");
+    const reviewsCollection = client.db("bistro").collection("reviews");
+    const   cartsCollection = client.db("bistro").collection("carts");
+    const   usersCollection = client.db("bistro").collection("users");
    
-   
+   app.get('/menu',async(req,res)=>{
+ const result = await menuCollection.find().toArray()
+ res.send(result)
+   })
+   app.get('/reviews',async(req,res)=>{
+  const result = await reviewsCollection.find().toArray()
+ res.send(result)
+   })
+// carts collection
+app.delete('/carts/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query= {_id : new ObjectId(id)}
+const result =await cartsCollection.deleteOne(query)
+res.send(result)
+
+})
+// users
+app.post('/users',async(req,res)=>{
+  const user = req.body;
+  const result = await usersCollection.insertOne(user)
+  res.send(result)
+})
+
+
+   app.post('/carts',async(req,res)=>{
+    const cartItem = req.body;
+    console.log(cartItem)
+    const result = await cartsCollection.insertOne(cartItem)
+    res.send(result)
+
+   })
+   app.get('/carts',async(req,res)=>{
+    const email = req.query.email;
+    const query = {email:email}
+    const result = await cartsCollection.find(query).toArray()
+   res.send(result)
+     })
 
   } finally {
     // Ensures that the client will close when you finish/error
@@ -42,3 +81,14 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro Boss is Counting ${port}`);
 });
+/**
+ * 
+ * 
+ * .....................................................
+ * 
+ * NAMEING CONVATION
+ * ........................................................
+ * 
+ * 
+ * 
+ */
