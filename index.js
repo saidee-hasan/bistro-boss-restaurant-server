d8380a9cab95e0b8d32de9d7c30b798e8e23dc2d
saideeWD <mdsaideehasan@gmail.com>
2025-01-10 17:03:44 +0600

    admin

--- index.js
+++ index.js
@@ -33,8 +33,49 @@ async function run() {
     const usersCollection = client.db("bistro").collection("users");
 
 
-// jwt tokenn
+// jwt token
+app.post('/jwt',async(req,res)=>{
+  const user = req.body;
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+ res.send({token});
 
+})
+//  middlewares
+const verifyToken = (req,res,next)=>{
+
+  if(!req.headers.authorization){
+    return  res.status(401).send({message:'forbidden access'})
+
+  }
+const token = req.headers.authorization.split(' ')[1]
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+  if(err){
+    return  res.status(401).send({message:'forbidden access'})
+  }
+  req.decoded = decoded;
+  next()
+})
+
+ 
+}
+
+
+app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+  const email = req.params.email;
+  if(email !== req.decoded.email){
+    return res.status(403).send({message:'unauthorize access'})
+  }
+
+  const query = {email:email};
+
+  const user = await usersCollection.findOne(query);
+let admin = false;
+if(user){
+  admin = user.role === 'admin'
+}
+res.send({admin})
+
+})
 
     app.delete('/users/:id',async(req,res)=>{
       const id = req.params.id;
@@ -80,7 +121,8 @@ app.patch('/users/admin/:id',async(req,res)=>{
 
 
     // menu releted api
-    app.get("/users", async (req, res) => {
+    app.get("/users",verifyToken, async (req, res) => {
+      console.log(req.headers)
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
