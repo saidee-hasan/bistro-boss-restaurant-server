c1ec8abfc9817fffe8f635139842cc1eca2967c7
saideeWD <mdsaideehasan@gmail.com>
2025-01-12 15:48:52 +0600

    add menu

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ app.post('/jwt',async(req,res)=>{
  res.send({token});
 
 })
+
+// verify admin  Token
+const verifyAdmin = async(req,res,next)=>{
+  const email  = req.decoded.email;
+  const query = {email : email}
+  const user = await usersCollection.findOne(query);
+  const isAdmin = user?.role === 'admin'
+  if(!isAdmin){
+    return  res.status(401).send({message:'forbidden access'})
+  }
+  next()
+
+}
+
 //  middlewares
 const verifyToken = (req,res,next)=>{
 
@@ -60,7 +74,7 @@ jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
 }
 
 
-app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+app.get('/users/admin/:email',verifyToken, async(req,res)=>{
   const email = req.params.email;
   if(email !== req.decoded.email){
     return res.status(403).send({message:'unauthorize access'})
@@ -77,7 +91,7 @@ res.send({admin})
 
 })
 
-    app.delete('/users/:id',async(req,res)=>{
+    app.delete('/users/:id', verifyToken,verifyAdmin,async(req,res)=>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)}
       const result = await usersCollection.deleteOne(query)
@@ -88,7 +102,7 @@ res.send({admin})
 
     })
 
-app.patch('/users/admin/:id',async(req,res)=>{
+app.patch('/users/admin/:id',verifyToken,verifyAdmin,async(req,res)=>{
   const id = req.params.id;
   const filter= {_id : new ObjectId(id)}
   const updateDoc ={
@@ -121,7 +135,7 @@ app.patch('/users/admin/:id',async(req,res)=>{
 
 
     // menu releted api
-    app.get("/users",verifyToken, async (req, res) => {
+    app.get("/users",verifyToken,verifyAdmin, async (req, res) => {
       console.log(req.headers)
       const result = await usersCollection.find().toArray();
       res.send(result);
